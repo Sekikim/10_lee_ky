15fb204c7a5b6119dde7bdfd877d04eed6f21809
Sekikim <132802611+Sekikim@users.noreply.github.com>
2023-05-07 18:53:09 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
